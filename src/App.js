8b51bccbe1af497a156d2e4ddf06cde7a88abf9f
GurkNathe <ethan.c.krug@gmail.com>
2021-06-21 13:30:14 -0700

    Used material-ui sign up template for sign up link on sign in page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // React-Bootstrap Components
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//Different components for front-end website
 import HeadBar from "./Components/Navbar";
 import About from "./Pages/About";
 import Competitions from "./Pages/Competitions";
@@ -11,18 +12,20 @@ import Home from "./Pages/Home";
 import Information from "./Pages/Information";
 import Login from "./Pages/Login";
 import Resources from "./Pages/Resources";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
     <Router>
         <HeadBar></HeadBar>
         <Switch>
-          <Route path="/about" exact component={About}/>
-          <Route path="/competitions" exact component={Competitions}/>
           <Route path="/" exact component={Home}/>
+          <Route path="/about" exact component={About}/>
           <Route path="/information" exact component={Information}/>
-          <Route path="/login" exact component={Login}/>
           <Route path="/resources" exact component={Resources}/>
+          <Route path="/competitions" exact component={Competitions}/>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/login/signup" exact component={SignUp}/>
         </Switch>
     </Router>
   );
