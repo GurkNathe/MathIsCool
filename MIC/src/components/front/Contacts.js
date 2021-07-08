02e85ddd2dd81695321c350b16f7ffd2cb3db289
GurkNathe <ethan.c.krug@gmail.com>
2021-07-08 13:19:33 -0700

    Finished History page. Started on Contacts page.

--- MIC/src/components/front/Contacts.js
+++ MIC/src/components/front/Contacts.js
@@ -1,9 +1,77 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core";
+import image from "../../assets/MIC_Regions.png";
+
+const useStyles = makeStyles((theme) => ({
+  td:{
+    border:"1px solid", 
+    width:"50%", 
+    borderRadius:"5px"
+  }
+}));
+
 function Contacts() {
+  const classes = useStyles();
   return (
-    <div className="Contacts">
-      <h1>Contacts</h1>
+    <div style={{display: "flex", flexDirection:"column",}}>
+      <div style={{margin:"2%", boxShadow:"0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%)"}}>
+        <div style={{marginLeft:"1%", marginRight:"1%"}}>
+          <h1 style={{fontStyle:"italic"}}>Contacts</h1>
+          <table>
+            <td>
+              <b>Registration Contact:</b><br/>
+              Gregg Sampson<br/>
+              Phone: (425)243-7188<br/>
+              Email: <a href="mailto:registrar@academicsarecool.com">
+                registrar@academicsarecool.com
+              </a>
+            </td>
+            <td>
+              <b>Payment Contact:</b><br/>
+              Academics Are Cool / Gina Petyuk<br/>
+              P.O. Box 2214, Richland, WA 99352<br/>
+              Email: <a href="mailto:treasurer@academicsarecool.com">
+                treasurer@academicsarecool.com
+              </a>
+            </td>
+          </table>
+          <p>Organization</p>
+          <div>
+            <b>President: </b>
+            <a href="mailto:president@academicsarecool.com">
+              Gregg Sampson
+            </a><br/>
+            <b>Vice President: </b>
+            <a href="">
+              Mahen Malixi
+            </a>
+          </div>
+          <table style={{width:"100%", border:"1px solid", borderRadius:"5px"}}>
+            <tr >
+              <td class={classes.td}>
+                <b>Regional Directors:</b><br/>
+                (<a href={image} target="_blank">Map of Regions/Sites</a>)
+              </td>
+              <td class={classes.td}>
+                <b>Sites</b>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
--- MIC/src/components/front/History.js
+++ MIC/src/components/front/History.js
@@ -2,10 +2,67 @@ import React from "react";
 
 function History() {
   return (
-    <div className="History">
-      <h1>History</h1>
+    <div style={{display: "flex", flexDirection:"row", position:"absolute", top:"134px", bottom:"0", zIndex: -1}}>
+      <div style={{margin:"2%", boxShadow:"0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%)"}}>
+        <div style={{marginLeft:"1%", marginRight:"1%"}}>
+          <h1 style={{fontStyle:"italic"}}>History</h1>
+          <p>
+            "Academics Are Cool" began in the spring of 1996 when Gregg 
+            Sampson, a mathematics teacher at Lewis & Clark High School 
+            (LCHS) in Spokane, Washington organized a mathematics 
+            competition for sixth grade students in the Spokane area. 
+            He and members of the LCHS Math Team organized the events, 
+            wrote the competition items and implemented the competition 
+            at LCHS.
+          </p>
+          <ul>
+            <li>
+              The following school year (1996/1997), the competition was 
+              expanded to grades 4-12 with grade level competitions held 
+              on six dates throughout the school year at LCHS. During the 
+              same school year, Triscia Hochstatter, a mathematics teacher 
+              at Moses Lake High School (MLHS), organized a grade four and 
+              five combined "Math Is Cool" competition in Moses Lake, adding 
+              more grade levels in subsequent years.
+            </li>
+            <li>
+              Beginning with the 1998/1999 school year, "Math Is Cool" 
+              competitions were held in Seattle, conducted by Gregg Sampson 
+              and Sean Ahern, a University of Washington student and LCHS 
+              alumnus. In 2002, Tom Tosch took over as the Seattle Regional 
+              Director.
+            </li>
+            <li>
+              Starting with the 2005/2006 school year, Wenatchee began hosting 
+              4-6th grade competitions.
+            </li>
+            <li>
+              In the 2008/2009 school year, the Vancouver region began hosting 
+              4th-12th grade competitions.
+            </li>
+            <li>
+              The next year (2010/2011), Tri-Cities began hosting 4-6th grades.
+            </li>
+            <li>
+              In 2015-2016, Coeur d’Alene began hosting 4th and 5th grades, 
+              adding 6th grade the next year.
+            </li>
+            <li>
+              In the 2017-2018 school year, the 7/8 Championships contest was 
+              held on the same date as the 6th grade Championships contest and 
+              both Tri-Cities and Wenatchee added the 7/8 contest.
+            </li>
+            <li>
+              In the year 2000-2001, the approximate number of students that 
+              competed was 3686. By the 2016-2017 school year, that number had 
+              grown to over 8000.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default History;
+
