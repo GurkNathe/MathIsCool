c91024035495884d2556c2cebd528f2d37f6fef7
GurkNathe <ethan.c.krug@gmail.com>
2022-08-05 13:07:06 -0700

    Fixed some things with sign up.

--- MIC/src/components/profile/SignUp.js
+++ MIC/src/components/profile/SignUp.js
@@ -17,9 +17,9 @@ import {
 } from "@firebase/auth";
 import { auth } from "../fire";
 
-//! Bug that allows you to change username, something wrong with onSubmit
 export default function SignUp() {
 	const history = useHistory();
+	// Holds the current state of the user sign up
 	const [up, setUp] = useState({
 		email: " ",
 		username: " ",
@@ -28,6 +28,11 @@ export default function SignUp() {
 		error: null,
 	});
 
+	/**
+	 * Handles the value inputs for the page
+	 * @param {object} event - The event that triggered the execution of the function
+	 * @param {string} type  - What field to update
+	 */
 	const onChange = (event, type) => {
 		switch (type) {
 			case "email":
@@ -70,13 +75,14 @@ export default function SignUp() {
 		}));
 	};
 
-	//will handle sending info to firebase and changing to loggedin page
-	const onSubmit = (event) => {
-		//Checks if password and confirmation password are the same.
+	// Will handle sending info to firebase and changing to logged in page
+	const onSubmit = () => {
+		// Checks if password and confirmation password are the same.
 		if (up.password !== up.confirm) {
 			setError(null);
 			setError("NoMatch");
 			return;
+			// If a non-valid username was provided
 		} else if (up.username.replace(/[^0-9a-z]/gi, "").length === 0) {
 			setError(null);
 			setError("NoUser");
@@ -85,39 +91,51 @@ export default function SignUp() {
 			//Sign's a person up using an email and password, and send email confirmation.
 			createUserWithEmailAndPassword(auth, up.email, up.password)
 				.then(() => {
+					// Sends email verification
 					sendEmailVerification(auth.currentUser);
+
+					// Sets session variables for user information
 					sessionStorage.setItem("email", up.email);
 					sessionStorage.setItem("username", up.username);
+
+					//Adds person's username
+					onAuthStateChanged(auth, (user) => {
+						if (user) {
+							updateProfile(user, {
+								displayName: up.username,
+							})
+								.then(() => {})
+								.catch((error) => {
+									setError(error.code);
+								});
+						}
+					});
+
+					// Email confirmation message
+					let message =
+						// eslint-disable-next-line no-multi-str
+						"An email was sent to your email address. Please \
+                        navigate to it and click the verification link. If \
+                        you don't see an email, it could take up to a few \
+                        minutes to be recieved. If the problem persists, \
+                        there is a resend confirmation button in the profile \
+                        page. If the problem continues, please contact an \
+                        administrator for help.";
+
+					// Routes to home page after a successful submission
 					history.push({
 						pathname: "/",
 						state: {
 							alert: true,
 							severity: "info",
-							message:
-								"An email was sent to your email address. Please navigate to it and click the verification link.",
-							duration: 5000,
+							message: message,
+							duration: 20000,
 						},
 					});
 				})
 				.catch((error) => {
 					if (up.error === null) setError(error.code);
 				});
-
-			//! need to enclose so no unwanted display name changes happen
-			//Adds person's username
-			const unsub = onAuthStateChanged(auth, (user) => {
-				if (user) {
-					updateProfile(user, {
-						displayName: up.username,
-					})
-						.then(() => {})
-						.catch((error) => {
-							setError(error.code);
-						});
-				}
-			});
-
-			return unsub;
 		}
 	};
 
@@ -135,7 +153,7 @@ export default function SignUp() {
 					<Grid container spacing={2}>
 						<Grid item xs={12}>
 							<TextField
-								error={up.error}
+								error={up.error === "NoUser"}
 								variant="outlined"
 								required
 								fullWidth
@@ -154,7 +172,10 @@ export default function SignUp() {
 
 						<Grid item xs={12}>
 							<TextField
-								error={up.error}
+								error={
+									up.error === "auth/invalid-email" ||
+									up.error === "auth/email-already-in-use"
+								}
 								variant="outlined"
 								required
 								fullWidth
@@ -175,7 +196,9 @@ export default function SignUp() {
 
 						<Grid item xs={12}>
 							<TextField
-								error={up.error}
+								error={
+									up.error === "NoMatch" || up.error === "auth/weak-password"
+								}
 								variant="outlined"
 								required
 								fullWidth
@@ -190,7 +213,9 @@ export default function SignUp() {
 
 						<Grid item xs={12}>
 							<TextField
-								error={up.error}
+								error={
+									up.error === "NoMatch" || up.error === "auth/weak-password"
+								}
 								variant="outlined"
 								required
 								fullWidth
@@ -202,7 +227,7 @@ export default function SignUp() {
 									up.error === "NoMatch"
 										? "Passwords did not match."
 										: up.error === "auth/weak-password"
-										? "Password should be at least 6 characters"
+										? "Password should be at least 6 characters."
 										: null
 								}
 								onChange={(event) => onChange(event, "confirm")}
